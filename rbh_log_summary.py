6e17dae7274798b4e01146d42eba17260758f728
Daniel Collins <daniel.collins@csiro.au>
2016-03-09 11:43:40 +0800

    added command line args

--- rbh_log_summary.py
+++ rbh_log_summary.py
@@ -1,48 +1,55 @@
 # coding: utf-8
+''' Generates summary reports from Robin Hood log files. '''
 
+import argparse
 import re
+import sys
+
+from humansize import approximate_size
 
 
-# Define the human-readable approximate size function, from Dive into Python 3
+if __name__ == '__main__':
 
-# Set up the regexes
-re_filename = re.compile(r".*Purged '([^']+)'")
-re_bytes = re.compile(r".*size=(\d+)")
-re_atime = re.compile(r".*last access (\d+\.\d+)")
+    # Set up the regexes
+    re_filename = re.compile(r".*Purged '([^']+)'")
+    re_bytes = re.compile(r".*size=(\d+)")
+    re_atime = re.compile(r".*last access (\d+\.\d+)")
 
-# values that could come from command-line args
-file = './askap_purge'
-atime_threshold = 30.0
-verbose = True
+    # command-line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', help='verbose output', action='store_true')
+    parser.add_argument('-a', '--atime_threshold', default=30.0, type=float)
+    parser.add_argument('log_file')
+    args = parser.parse_args()
 
-# report variables
-total_bytes = 0.0
-bad_log_lines = 0
-total_files = 0
+    # report variables
+    total_bytes = 0.0
+    bad_log_lines = 0
+    total_files = 0
 
-# parse the log line by line
-with open(file) as f:
-    for line in f:
-        try:
-            size_bytes = (int)(re_bytes.match(line).group(1))
-            atime = (float)(re_atime.match(line).group(1))
+    # parse the log line by line
+    with open(args.log_file) as f:
+        for line in f:
+            try:
+                size_bytes = (int)(re_bytes.match(line).group(1))
+                atime = (float)(re_atime.match(line).group(1))
 
-            if atime >= atime_threshold:
-                total_bytes += size_bytes
-                total_files += 1
+                if atime >= args.atime_threshold:
+                    total_bytes += size_bytes
+                    total_files += 1
 
-            if verbose:
-                filename = re_filename.match(line).group(1)
-                print('{0}: {1} bytes, last access: {2} days'.format(filename, size_bytes, atime))
-        except e:
-            bad_log_lines++
-            continue
+                    # filename = re_filename.match(line).group(1)
+            except:
+                if args.verbose:
+                    e = sys.exc_info()[0]
+                    print(e)
 
+                bad_log_lines += 1
+                continue
 
-# Final report
-print('Source log file: {0}'.format(file))
-print('Bad log lines (skipped): {0}'.format(bad_log_lines))
-print('Threshold: {0} days'.format(atime_threshold))
-print('File count: {0}'.format(total_files))
-print('Total freed: {0}'.format(total_bytes))
-
+    # Final report
+    print('Source log file: {0}'.format(args.log_file))
+    print('Bad log lines (skipped): {0}'.format(bad_log_lines))
+    print('Threshold: {0} days'.format(args.atime_threshold))
+    print('File count: {0}'.format(total_files))
+    print('Total freed: {0}'.format(approximate_size(total_bytes)))
