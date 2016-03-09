0f44361581eded4f9f79e93ad6c5aedce203f915
Daniel Collins <daniel.collins@csiro.au>
2016-03-09 14:45:29 +0800

    adjusted walltime based on initial tests

--- run.sh
+++ run.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 #SBATCH --job-name=Dry-run Purge log summariser
-#SBATCH --account=
-#SBATCH --partition=
+#SBATCH --account=astronomy856
+#SBATCH --partition=workq
 #SBATCH --nodes=1
-#SBATCH --time=01:00:00
+#SBATCH --time=00:05:00
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 
-module load python
+module load python/2.7.10
 
-LOG_FILE=/scratch2/PurgeTest
-MAX_RECORDS=10000
+LOG_FILE=/scratch2/PurgeTest/askap_2016-03-03
+MAX_RECORDS=1000000
 
 aprun -n 1 \
+time \
 python ./rh_log_summary.py \
 --atime_threshold 30.0 \
 --max_records $MAX_RECORDS \
