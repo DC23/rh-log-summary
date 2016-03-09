33166d4fdb66ded451941fddc293214fe014658e
Daniel Collins <daniel.collins@csiro.au>
2016-03-09 15:27:47 +0800

    changing run to do multiple reports at a time

---
+++ batch_report.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -login
+module load python/2.7.10
+
+LOG_FILE=/scratch2/PurgeTest/askap_2016-03-03
+#LOG_FILE=/scratch2/PurgeTest/mwa_2016-03-03
+
+echo ----------------
+python ./rh_log_summary.py --atime_threshold 30.0 $LOG_FILE
+echo ----------------
+python ./rh_log_summary.py --atime_threshold 40.0 $LOG_FILE
+echo ----------------
+python ./rh_log_summary.py --atime_threshold 50.0 $LOG_FILE
+echo ----------------
+python ./rh_log_summary.py --atime_threshold 60.0 $LOG_FILE
+echo ----------------
--- run.sh
+++ run.sh
@@ -3,17 +3,9 @@
 #SBATCH --account=astronomy856
 #SBATCH --partition=workq
 #SBATCH --nodes=1
-#SBATCH --time=00:05:00
+#SBATCH --time=00:10:00
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 
-module load python/2.7.10
-
-LOG_FILE=/scratch2/PurgeTest/askap_2016-03-03
-
-aprun -n 1 \
-time \
-python ./rh_log_summary.py \
---atime_threshold 30.0 \
-$LOG_FILE
+aprun -n 1 ./batch_report.sh
