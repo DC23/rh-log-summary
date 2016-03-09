2851280e5e4afbb0ccfb4c0c9cfdf49e68b6dea1
Daniel Collins <daniel.collins@csiro.au>
2016-03-09 14:53:42 +0800

    removing max records from run script

--- run.sh
+++ run.sh
@@ -11,11 +11,9 @@
 module load python/2.7.10
 
 LOG_FILE=/scratch2/PurgeTest/askap_2016-03-03
-MAX_RECORDS=1000000
 
 aprun -n 1 \
 time \
 python ./rh_log_summary.py \
 --atime_threshold 30.0 \
---max_records $MAX_RECORDS \
 $LOG_FILE
