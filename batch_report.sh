6606844f200b140c7468e69d17ee05eb5e5f6436
Daniel Collins <daniel.collins@csiro.au>
2016-05-30 14:37:01 +0800

    added additional time windows to report

--- batch_report.sh
+++ batch_report.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -login
 module load python/2.7.10
 
-LOG_FILE=/scratch2/PurgeTest/askap_2016-03-03
+LOG_FILE=/scratch2/PurgeTest/askap_2016-05-26
 #LOG_FILE=/scratch2/PurgeTest/mwa_2016-03-03
 
 echo Report for $LOG_FILE
+echo ----------------
+python ./rh_log_summary.py --atime_threshold 10.0 $LOG_FILE
+echo ----------------
+python ./rh_log_summary.py --atime_threshold 20.0 $LOG_FILE
 echo ----------------
 python ./rh_log_summary.py --atime_threshold 30.0 $LOG_FILE
 echo ----------------
@@ -13,4 +17,8 @@ echo ----------------
 python ./rh_log_summary.py --atime_threshold 50.0 $LOG_FILE
 echo ----------------
 python ./rh_log_summary.py --atime_threshold 60.0 $LOG_FILE
+echo ----------------
+python ./rh_log_summary.py --atime_threshold 75.0 $LOG_FILE
+echo ----------------
+python ./rh_log_summary.py --atime_threshold 90.0 $LOG_FILE
 echo ----------------
